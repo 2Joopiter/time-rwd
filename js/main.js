3cd7f3dc3a831d04fb7e9cb7401d3ce453d89bef
2joopiter <veryberrylike3@gmail.com>
2023-11-15 11:29:18 +0900

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const screen = document.querySelector('.screen');
+const [spanHr, spanMin, spanSec] = screen.querySelectorAll('span');
+
+setInterval(() => {
+	const now = new Date();
+	let hr = now.getHours();
+	let min = now.getMinutes();
+	let sec = now.getSeconds();
+
+	spanHr.innerText = hr < 10 ? '0' + hr : hr;
+	spanMin.innerText = min < 10 ? '0' + min : min;
+	spanSec.innerText = sec < 10 ? '0' + sec : sec;
+}, 1000);
