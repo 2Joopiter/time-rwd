839e2e5f59b93ff6283de8b4ed85b7d94c8dda78
2joopiter <veryberrylike3@gmail.com>
2023-11-15 11:36:51 +0900

    ampm 시간에 따른 변화 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const screen = document.querySelector('.screen');
 const [spanHr, spanMin, spanSec] = screen.querySelectorAll('span');
+const em = screen.querySelectorAll('em');
 
 setInterval(() => {
 	const now = new Date();
@@ -7,6 +8,7 @@ setInterval(() => {
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
 
+	em.innerText = hr < 12 ? 'am' : 'pm';
 	spanHr.innerText = hr < 10 ? '0' + hr : hr;
 	spanMin.innerText = min < 10 ? '0' + min : min;
 	spanSec.innerText = sec < 10 ? '0' + sec : sec;
