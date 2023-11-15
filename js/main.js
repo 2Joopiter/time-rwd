02bcea688953aca868f389a936c0ccedc5fe4a0b
2joopiter <veryberrylike3@gmail.com>
2023-11-15 12:42:49 +0900

    테마 변경함수 추가

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+const main = document.querySelector('main');
 const screen = document.querySelector('.screen');
 const em = screen.querySelector('em');
 const numbers = screen.querySelectorAll('span');
 
 setInterval(() => {
+	changeTheme();
 	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
 	getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
@@ -24,4 +26,23 @@ function getTime() {
 
 function setTime(num, index) {
 	numbers[index].innerText = num < 10 ? '0' + num : num;
+}
+
+// 시간에 따른 테마 변경 함수
+function changeTheme() {
+	const hr = new Date().getHours();
+	main.className = '';
+
+	if (hr >= 5 && hr < 10) {
+		main.classList.add('morning');
+	}
+	if (hr >= 10 && hr < 16) {
+		main.classList.add('afternoon');
+	}
+	if (hr >= 16 && hr < 20) {
+		main.classList.add('evening');
+	}
+	if (hr >= 20 || hr < 5) {
+		main.classList.add('night');
+	}
 }
