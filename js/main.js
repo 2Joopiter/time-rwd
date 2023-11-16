776d37c60708c3413167b3fb1065d8f6829b1251
2joopiter <veryberrylike3@gmail.com>
2023-11-16 09:56:00 +0900

    메뉴버튼 클릭시 자동테마변경 기능 종료 및 수동으로 테마변경

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const main = document.querySelector('main');
 const screen = document.querySelector('.screen');
 const em = screen.querySelector('em');
 const numbers = screen.querySelectorAll('span');
+const btns = document.querySelectorAll('nav span');
 
 const data = [
 	{ cond: new Date().getHours() >= 5 && new Date().getHours() < 10, name: 'morning' },
@@ -10,11 +11,31 @@ const data = [
 	{ cond: new Date().getHours() >= 20 || new Date().getHours() < 5, name: 'night' },
 ];
 
-setInterval(() => {
+//여기까지 전역변수
+
+let timer = setInterval(() => {
 	changeTheme(data);
 	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
 	getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
+
+//이벤트1 (함수호출)
+
+btns.forEach((btn) => {
+	// 각 버튼 클릭시
+	btn.addEventListener('click', (e) => {
+		// 클릭한 버튼만 활성화
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.currentTarget.classList.add('on');
+
+		// 이하 자동 롤링기능 끊어주는 기능 (setInterval을 끊어줌)
+		clearInterval(timer);
+		//메인요소의 모든 클래스 제어
+		main.className = '';
+		//클릭한 버튼의 글자를 가져와서 소문자로 변경한 다음 메인요소의 클래스명으로 지정
+		main.classList.add(e.currentTarget.innerText.toLowerCase());
+	});
+});
 
 function getTime() {
 	const now = new Date();
@@ -30,10 +51,13 @@ function setTime(num, index) {
 	numbers[index].innerText = num < 10 ? '0' + num : num;
 }
 
-function changeTheme(info) { 
+function changeTheme(info) {
 	main.className = '';
 
-	data.forEach((el) => {
+	info.forEach((el) => {
 		if (el.cond) main.classList.add(el.name);
 	});
 }
+
+// 코드를 짤 때: 머릿속으로 내가 할 작업을 목록화.
+// 자동 반복기능 끊기 > 메뉴 클릭 > 클릭할 버튼을 가져와서 / class를 바꿔치기
